850ab21b128fe3f3f9bd8d29b46da0a20ddba369
RustySeynt <125332033+RustySeynt@users.noreply.github.com>
2023-06-15 11:31:23 +0800

    Add files via upload

---
+++ login.php
@@ -0,0 +1,23 @@
+<?php
+
+include_once('db.php');
+include_once('request.php');
+include('cors.php');
+
+$requestType = $_SERVER['REQUEST_METHOD'];
+
+if (!check("POST", $requestType)) {
+    echo "Error: 403 Forbidden Access";
+    exit();
+}
+ 
+$data = json_decode(file_get_contents('php://input'), true);
+
+$sql = 'SELECT * FROM users WHERE email="'.$data["email"].'" AND password="'.$data["password"].'"';
+$result = $mysqli->query($sql); 
+
+if($result->num_rows > 0){
+    echo json_encode(["status" => true, "message" => "account found"]);
+}else{
+    echo json_encode(["status" => false, "message" => "account not found"]);
+}
